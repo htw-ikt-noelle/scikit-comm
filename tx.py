ff41d7c493570dff70c2156485d1b83bbc38eea7
noelle <noelle@htw-berlin.de>
2020-06-05 17:06:08 +0200

    added first draft of generate_bits

--- tx.py
+++ tx.py
@@ -3,6 +3,43 @@ import scipy.signal as signal
 import matplotlib.pyplot as plt
 from . import utils
 
+
+
+def generate_bits(n_bits=2**15, type='random', seed=None):
+    """
+    Generate an array of size (n_bits,) binary values.
+
+    Parameters
+    ----------
+    n_bits : TYPE, optional
+        DESCRIPTION. The default is 2**15.
+    type : TYPE, optional
+        DESCRIPTION. The default is 'random'.
+    seed : TYPE, optional
+        DESCRIPTION. The default is None.
+
+    Raises
+    ------
+    ValueError
+        DESCRIPTION.
+
+    Returns
+    -------
+    bits : TYPE
+        DESCRIPTION.
+
+    """
+    
+    if type == 'random':
+        rng = np.random.default_rng(seed=seed)
+        bits = rng.integers(0, high=2, size=n_bits, dtype=bool)
+    else:
+        raise ValueError('type not implemented yet...')
+    
+    return bits
+
+
+
 def mapper(bits, constellation):
     """ Map bits to a given constellation alphabet.
 	
