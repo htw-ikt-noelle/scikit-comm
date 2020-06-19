22d489ee8f5abd545d8faaa46bda3fb53db2686e
noelle <noelle@htw-berlin.de>
2020-06-19 12:33:07 +0200

    added batch script for generating docs

---
+++ docs/make.bat
@@ -0,0 +1,23 @@
+@ECHO OFF 
+
+pushd %~dp0 
+
+REM Command file for Sphinx documentation 
+
+set SPHINXBUILD = sphinx-build
+set SOURCEDIR= source
+set BUILDDIR= build
+
+if "%1" == "" goto help
+
+
+%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O% 
+goto end 
+
+:help 
+%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O% 
+
+:end 
+
+popd
+
