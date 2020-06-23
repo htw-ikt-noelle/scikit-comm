ecc3a7442659dc36e55a95c0747f16a100edf849
noelle <noelle@htw-berlin.de>
2020-06-23 15:32:08 +0200

    started implementation of signal, still work to be done

--- awgDriver.py
+++ awgDriver.py
@@ -5,28 +5,67 @@ Created on Tue Jun 23 00:01:25 2020
 @author: Prabesh
 """
 
-def set_samples_AWG33522A(channels=(1), address = '192.168.1.45'):
+def set_samples_AWG33522A(self,channels=(1), address = '192.168.1.44'):
     import visa 
     import numpy as np
+    from scipy import signal as sg
     import comm as comm
-    
+        
     rm = visa.ResourceManager('@py')
     awg = rm.open_resource('TCPIP::' + address + '::INSTR')
-    idn = awg.ask('*IDN?')
+    idn = awg.query('*IDN?')
     print(idn)
     
     sig = comm.signal.Signal(n_dims=2)
     sig.symbol_rate = 32e9
 	
-	running = float(awg.query('STATus:CONDition?')) % 2
+	running = float(awg.query('*STB?')) % 2
     
     if running:
-		awg.write('TRIGger:MODE SINGle')
+		awg.write('TRIGger:SOUR CH1')
         busy = 1
         while busy:
-            busy = float(awg.query('STATus:CONDition?')) % 2
+            busy = float(awg.query('*STB?')) % 2
 			
-	# sig = np.array([])
+	wfm = np.array([])
+    
+   
+   
+   # if wfm is SIN, SQUARE, RAMP, TRIANg,PULSE ,PRBSPNX , ARB
+   
+   if wfm == np.SIN: 
+       print('This is a sinusoid ')
+       return
+   if wfm == scipy.signal.SQUARE:
+       print('This is a square wave')
+       return
+   if wfm ==  'ARB':
+       if len(wfm)<2 or len(wfm)>2**17:
+           print('ERROR!! length of waveform must be between 2 and 131072 points...')
+           return
+       awg.MEM = 'ARB1'
+       interpolate = ON # OFF
+   
+    if ip != address:
+        print('Error: IP Adress could not be found, Please check and run again')
+        
+    #hasattr(x, 'foo')
+    
+    if waveform == 'ARB':
+        if len(sig) < 2 or len(sig) > 2**17:
+            print('Error at signal, must be between 2 and 131072 points')
+            return
+        
+        self.sig = sig - min(sig)
+        self.sig = sig ./ max(sig) .* 2**14
+        self.sig = unit16(sig)
+    
+    
+    
+    
+    for idx, channel in enumerate(channels):
+        
+        awg.write('')
 	
 	if len(sig) < 2 or len(sig) > 2**17
 		print('Error at signal, must be between 2 and 131072 points')
