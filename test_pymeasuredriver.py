cc607c6cd690b593d51e315ec03f94e3f353004e
noelle <noelle@htw-berlin.de>
2020-07-02 15:24:56 +0200

    few changes,might not be used as a driver

--- test_pymeasuredriver.py
+++ test_pymeasuredriver.py
@@ -11,25 +11,46 @@ import sys
 sys.path.append("D:/Python/pymeasure")       #from lab
 #sys.path.append("C:/PythonSignal/pymeasure") #from home
 import pymeasure
-from pymeasure.instruments.agilent import Agilent33521A
+from pymeasure.instruments.agilent import Agilent33500
 from pymeasure.adapters import VISAAdapter  # importing Visaadapters from pymeasure
 
 print(pymeasure.__version__)
 
 adapter = VISAAdapter('TCPIP::192.168.1.44::INSTR') # interface for the adapter must be defined
-awg = Agilent33521A(adapter) 
+awg = Agilent33500(adapter) 
 
 awg.id
+"""awg.id
+Out[11]: ['Agilent Technologies', '33522A', 'MY50002322', '5.02-1.19-2.00-58-00']"""
 
 # Value of SQUARE is not in the discrete set ['SIN', 'SQU', 
 #'TRI', 'RAMP', 'PULS', 'PRBS', 'NOIS', 'ARB', 'DC']
 
-awg.shape = "SQU" # or 'SQU'
-#awg.beep() # gives beep output
-awg.output = True # selects the channel 1 output True = on, False = off
-awg.frequency = 1e-3
-awg.amplitude = 10
+#awg.shape = "SQU" # or 'SQU'
+# #awg.beep() # gives beep output
+#awg.output = True # selects the channel 1 output True = on, False = off
+awg.frequency = 100
+#awg.amplitude = 10
 awg.amplitude_unit = 'VPP' #VRMS, DBM
-awg.offset = 3.5
+#awg.offset =2
 
+awg.shape = (input("Which function would you like to enter?  :  "))
+
+try: 
+    functionstr = str(awg.shape)
+    print("The function you chose is:", awg.shape)
+    
+except ValueError as e:
+    e
+
+else :
+    print ( " Try Again ")
+    awg.shape = (input("Which function would you like to enter?  :  "))
+
+
+    
+
+    
+    
+   
 
