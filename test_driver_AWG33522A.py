2945465242bcd1085191d43aa6381e7fcf3e7f75
noelle <noelle@htw-berlin.de>
2020-07-10 16:37:13 +0200

    implemented class in order check attributes for driver

--- test_driver_AWG33522A.py
+++ test_driver_AWG33522A.py
@@ -9,24 +9,23 @@ import numpy as np
 import random
 from writeWfmToAWG33522A import write_samples_AWG33522A
 
+class user_attributes:
+    ipAdress = '192.168.1.44'
+    channels = [1,2]
+    
+    def __init__(self):
+        self.Amp_PP = Amp_PP
+        self.Offset = Offset
+        self.SampleRate = SampleRate
+        self.nSamples = nSamples
 
-
-channels = [1]
-ipAdress = '192.168.1.44'
-Amp_PP = 3.0
-Offset = 0.0
-SampleRate = 52.5e6
-nSamples = 100000
+user_attributes.nSamples = 100000
 
 #sample generation using numpy array method
 # random float samples generated, clipped between -1 and 1 and multiplied by 32767 and set type as integer
-array_samples= np.round(np.clip(np.random.randn(1,nSamples),-1.0,1.0)*32767).astype(int)
+array_samples= np.round(np.clip(np.random.randn(1,user_attributes.nSamples),-1.0,1.0)*32767).astype(int)
 list_samples = array_samples.tolist() # changes array into list -- shape [[x,y,z...]]
 samples = list_samples.pop() # pops the list out of the list -- shapes[ x,y,z...]
-
-
-
-
 
 
 
