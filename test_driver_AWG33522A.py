1aff0c613e155c6ebc62470b88456fe936e50345
noelle <noelle@htw-berlin.de>
2020-07-15 15:37:00 +0200

    minor changes in both files

--- test_driver_AWG33522A.py
+++ test_driver_AWG33522A.py
@@ -10,7 +10,6 @@ import random
 from writeWfmToAWG33522A import write_samples_AWG33522A
 
 class user_attributes:  
-    
     def __init__(self):
         self.ipAdress = ipAdress
         self.Amp_PP = Amp_PP
@@ -18,30 +17,26 @@ class user_attributes:
         self.SampleRate = SampleRate
         self.nSamples = nSamples
         self.Offset = Offset
-        self.channels = channels
+        self.channel= channel
 
-# addresing the attributes
+# setting up default values
 user_attributes.ipAdress = '192.168.1.44'
-user_attributes.nSamples = 50000  # number of samples
-user_attributes.SampleRate = 72e6 # not more than 250e6
-user_attributes.Offset = 3.0      # range -5V +5V
-user_attributes.Amp_PP = 2.0      #range 1e-3 to +10V
-user_attributes.channels = 1 # channels can be 1 or 2 as specified by the AWG
+user_attributes.nSamples = int(1e6)  # number of samples
+user_attributes.SampleRate = 250e6 # not more than 250e6
+user_attributes.Offset = 0.0      # range -5V +5V
+user_attributes.Amp_PP = 0.0      #range 1e-3 to +10V
+user_attributes.channel = 1 # channels can be 1 or 2 as specified by the AWG
 
 
 #generating samples for 1st channel
 # random float samples generated, clipped between -1 and 1 and multiplied by 32767 and set type as integer
-array_samples1= np.round(np.clip(np.random.randn(1,user_attributes.nSamples),-1.0,1.0)*32767).astype(int)
-samples1 = array_samples1.tolist().pop() # changes array into list -- shape [[x,y,z...]]
+array_samples= np.round(np.clip(np.random.randn(1,user_attributes.nSamples),-1.0,1.0)*32767).astype(int)
+samples = array_samples.tolist().pop() # changes array into list -- shape [[x,y,z...]]
 # samples = list_samples.pop() # pops the list out of the list -- shapes[ x,y,z...]
 
 
-# generating samples for channel 2
-array_samples2= np.round(np.clip(np.random.randn(1,user_attributes.nSamples),-1.0,1.0)*32767).astype(int)
-samples2 = array_samples2.tolist().pop() # changes array into list -- shape [[x,y,z...]]
-
-# channel 1 output, input parameters can be changed
-write_samples_AWG33522A(user_attributes, samples1, samples2, SampleRate= 240e6 ,channels = 1, Amp_PP = 4.0, Offset= 1.0, filter_settings = "normal")
+# channel 1 output, input parameters can be changed, filters can be set to normal, off or step, cases can be ignored
+write_samples_AWG33522A(user_attributes, samples, SampleRate=250e6 ,channel = 1, Amp_PP = 2.0, Offset= 0.0, filter_settings = "off")
 #channel 2 output
-write_samples_AWG33522A(user_attributes, samples1, samples2, SampleRate= 240e6 ,channels = 2, Amp_PP = 3.0, Offset= 0.0, filter_settings = "normal")
+write_samples_AWG33522A(user_attributes, samples, SampleRate=250e6 ,channel = 2, Amp_PP = 4.0, Offset= 0.0, filter_settings = "off")
 
--- writeWfmToAWG33522A.py
+++ writeWfmToAWG33522A.py
@@ -10,16 +10,9 @@ import math
 import matplotlib.pyplot as plt
 # import comm
 
-# from comm import signal
-# from comm import instrument_control
 
-# !! to DO !!
-
-"""
-to do is set different parameters such as samplerates, amplitudes for both channels and setup differently for both channels, channels can be selected by giving channesl name"""
-
-#def write_samples_AWG33522A(user_inputs, ipAdress,channels,nSamples,samples,SampleRate,Amp_PP, Offset, filter_settings = None):
-def write_samples_AWG33522A(user_attributes, samples1, samples2, SampleRate= 72e6 ,channels = 2, Amp_PP = 3.0, Offset= 1.0, filter_settings= "normal"): 
+#def write_samples_AWG33522A(user_inputs, ipAdress,channel,nSamples,samples,SampleRate,Amp_PP, Offset, filter_settings = None):
+def write_samples_AWG33522A(user_attributes, samples, SampleRate=62.5e6 ,channel=1, Amp_PP=3.0, Offset=1.0, filter_settings="normal"): 
     
 
 # =============================================================================
@@ -29,8 +22,8 @@ def write_samples_AWG33522A(user_attributes, samples1, samples2, SampleRate= 72e
     if hasattr(user_attributes,'ipAdress') == False:
         raise ValueError ('ipAdress has to be specified...')
         
-    if hasattr(user_attributes, 'channels') == False:
-        raise ValueError ('channels has to be specified...')
+    if hasattr(user_attributes, 'channel') == False:
+        raise ValueError ('channel has to be specified...')
     
     if hasattr(user_attributes, 'Amp_PP') == False:
         raise ValueError ('Amplitude has to be specified...')
@@ -53,15 +46,16 @@ def write_samples_AWG33522A(user_attributes, samples1, samples2, SampleRate= 72e
     if Offset < -5 or Offset > 5 :
         raise ValueError(' min -5V and max +5V into 50 ohm impedance')
         
+    if SampleRate > 62.5e6 and filter_settings =="off":
+        raise ValueError('For OFF filter Settings, sample rate should be lower than 62.5 Million samples per second')
+        
 # =============================================================================
 #  importing visa for communication with the device
 # =============================================================================
              
     import visa
-    
     # create resource 
     rm = visa.ResourceManager('@py')
-    
     # open connection to AWG
     awg = rm.open_resource('TCPIP::' + getattr(user_attributes,'ipAdress') + '::INSTR')
    
@@ -69,29 +63,27 @@ def write_samples_AWG33522A(user_attributes, samples1, samples2, SampleRate= 72e
 #    setting up channel 1 
 # =============================================================================
    
-    if channels == 1:
-        
+    if channel == 1:
         # selecting byte order , used to make binary data point transfers in the block mode Swapped(LSB) or Normal(MSB)
         # SWAPped byte order,(LSB) of each data point is assumed first. Most computers use the "swapped" byte order.
-   
         awg.write(':FORMat:BORDer %s' % ('SWAPped'))
            
-        if SampleRate > 62.5e6 and filter_settings =="off":
-            raise ValueError('For OFF filter Settings, sample rate should be lower than 62.5 Million samples per second')
+        # if SampleRate > 62.5e6 and filter_settings =="off":
+        #     raise ValueError('For OFF filter Settings, sample rate should be lower than 62.5 Million samples per second')
     
         # clearing the waveform memory of the specified channel, 1 or 2 if any.
         awg.write(':SOURce1:DATA:VOLatile:CLEar')
     
         # writing values representing DAC codes into waveform volatile memory, as binary block data/ list of integer samples from -32767 to +32767.
         # loading data into the AWG as arb1, can be named anything, just need to check source number 1 or 2
-        awg.write_binary_values(':SOURce1:DATA:ARBitrary:DAC %s,' % ('arb1' ),samples1, datatype = 'h', is_big_endian = False )
+        awg.write_binary_values(':SOURce1:DATA:ARBitrary:DAC %s,' % ('arb1' ),samples, datatype = 'h', is_big_endian = False )
         
         # setting function as ARB, data will be saved under arb1 file
         awg.write(':SOURce1:FUNCtion:SHAPe:ARBitrary "%s"' % ('arb1'))
         
         # applying sample rate, amplitude and Offset
-        awg.write(':SOURce1:APPLy:ARBitrary %G,%G,%G' % (SampleRate, Amp_PP, Offset))
-        
+        #awg.write(':SOURce1:APPLy:ARBitrary %G,%G,%G' % (SampleRate, Amp_PP, Offset))
+        awg.write(':SOURce%d:APPLy:ARBitrary %s,%s,%s' % (channel, SampleRate, Amp_PP, Offset))
         # synchronising both channels to the first point one channel should be OK!
         
         awg.write(':SOUR1:FUNCtion:ARBitrary:SYNC ')
@@ -114,27 +106,26 @@ def write_samples_AWG33522A(user_attributes, samples1, samples2, SampleRate= 72e
 #      setting up channel 2       
 # =============================================================================
             
-    if channels == 2:
+    if channel == 2:
         
         awg.write(':FORMat:BORDer %s' % ('SWAPped'))
-        
-        if SampleRate > 62.5e6 and filter_settings =="off":
-            raise ValueError('For OFF filter Settings, sample rate should be lower than 62.5 Million samples per second')
+       
         awg.write(':SOURce2:DATA:VOLatile:CLEar')
         
-        awg.write_binary_values(':SOURce2:DATA:ARBitrary:DAC %s,' % ('arb2' ),samples2, datatype = 'h', is_big_endian = False )
+        awg.write_binary_values(':SOURce2:DATA:ARBitrary:DAC %s,' % ('arb2' ),samples, datatype = 'h', is_big_endian = False )
         
         # setting function as ARB, data will be saved under arb2 file
         awg.write(':SOURce2:FUNCtion:SHAPe:ARBitrary "%s"' % ('arb2'))
         
         # applying sample rate, amplitude and Offset
-        awg.write(':SOURce2:APPLy:ARBitrary %G,%G,%G' % (SampleRate, Amp_PP, Offset))
+        #awg.write(':SOURce2:APPLy:ARBitrary %G,%G,%G' % (SampleRate, Amp_PP, Offset))
+        awg.write(':SOURce%d:APPLy:ARBitrary %s,%s,%s' % (channel, SampleRate, Amp_PP, Offset))
         
         # synchronising both channels to the first point one channel should be OK!
         
         awg.write(':SOUR2:FUNCtion:ARBitrary:SYNC ')
         
-        if SampleRate <= 62.5e6 and filter_settings.lower() == "off" and channels == 2:
+        if SampleRate <= 62.5e6 and filter_settings.lower() == "off" and channel == 2:
             filter_gen = awg.write('SOUR2:FUNCtion:ARBitrary:FILTer OFF ')
             awg.write(':SOURce2:FUNCtion:SHAPe:ARBitrary:SRATe %G' % (SampleRate))
   
