d5167e55a609a6611dbc36eb1358353504802ee3
Marcel <s30308@beuth-hochschule.de>
2021-09-23 17:55:25 +0200

    Added test case for different sample rates

--- setup_files/Test_write_samples_AWG70002B.py
+++ setup_files/Test_write_samples_AWG70002B.py
@@ -11,73 +11,88 @@ import comm as comm
 import time
 
 
-# Test with one signal
-n = 1000
-phi = np.arange(n)/n * np.pi*2
-samples = np.array([np.cos(phi)])
+#--- Test with one signal
+# n = 1000
+# phi = np.arange(n)/n * np.pi*2
+# samples = np.array([np.cos(phi)])
 
 
-# write samples to AWG
-comm.instrument_control.write_samples_Tektronix_AWG70002B(samples, ip_address='192.168.1.21',
-                                                sample_rate=[1e6],
-                                                amp_pp=[0.5], channels=[1], 
-                                                out_filter=['normal'],log_mode=True)
+# # write samples to AWG
+# comm.instrument_control.write_samples_Tektronix_AWG70002B(samples, ip_address='192.168.1.21',
+#                                                 sample_rate=[1e6],
+#                                                 amp_pp=[0.5], channels=[1], 
+#                                                 out_filter=['normal'],log_mode=True)
 
-'''
-# Test with two signals
-n = 1000
-phi = np.arange(n)/n * np.pi*2
-samples = np.array([np.cos(phi),np.sin(phi)])
 
-# write samples to AWG
-comm.instrument_control.write_samples_Tektronix_AWG70002B(samples, ip_address='192.168.1.21',
-                                                sample_rate=[500e6],
-                                                amp_pp=[0.5,0.3], channels=[1,2], 
-                                                out_filter=['normal'],log_mode=True)
-'''
-'''
-# Test with NaN in sample vector
-n = 1000
-phi = np.arange(n)/n * np.pi*2
-samples = np.array([np.cos(phi)])
-samples = np.array([np.append(samples,np.nan)])
+#--- Test with different sampling rates
+# n = 1000
+# phi = np.arange(n)/n * np.pi*2
+# samples = np.array([np.cos(phi),np.sin(phi)])
 
-# write samples to AWG
-comm.instrument_control.write_samples_Tektronix_AWG70002B(samples, ip_address='192.168.1.21',
-                                                sample_rate=[500e6],
-                                                amp_pp=[0.5], channels=[1], 
-                                                out_filter=['normal'],log_mode=True)
+
+# for srates in list(range(1,10)):
+#     # write samples to AWG
+#     comm.instrument_control.write_samples_Tektronix_AWG70002B(samples, ip_address='192.168.1.21',
+#                                                     sample_rate=[srates*10**6],
+#                                                     amp_pp=[0.5,0.3], channels=[1,2], 
+#                                                     out_filter=['normal'],log_mode=True)
+
+#--- Test with two signals
+
+# n = 1000
+# phi = np.arange(n)/n * np.pi*2
+# samples = np.array([np.cos(phi),np.sin(phi)])
+
+# # write samples to AWG
+# comm.instrument_control.write_samples_Tektronix_AWG70002B(samples, ip_address='192.168.1.21',
+#                                                 sample_rate=[500e6],
+#                                                 amp_pp=[0.5,0.3], channels=[1,2], 
+#                                                 out_filter=['normal'],log_mode=True)
+
+
+#--- Test with NaN in sample vector
+# n = 1000
+# phi = np.arange(n)/n * np.pi*2
+# samples = np.array([np.cos(phi)])
+# samples = np.array([np.append(samples,np.nan)])
+
+# # write samples to AWG
+# comm.instrument_control.write_samples_Tektronix_AWG70002B(samples, ip_address='192.168.1.21',
+#                                                 sample_rate=[500e6],
+#                                                 amp_pp=[0.5], channels=[1], 
+#                                                 out_filter=['normal'],log_mode=True)
 
 # Result: The NaN will be played by the AWG with maximum amplitude.
 #         Also the AWG will give a warning: "Waveform contains invalid sample values" at his HOME screen.
 #         The driver should prevent this by not accepting NaN values
 
 # Test with Inf in sample vector
-n = 1000
-phi = np.arange(n)/n * np.pi*2
-samples = np.array([np.sin(phi)])
-samples = np.array([np.append(samples,np.Inf)])
+# n = 1000
+# phi = np.arange(n)/n * np.pi*2
+# samples = np.array([np.sin(phi)])
+# samples = np.array([np.append(samples,np.Inf)])
 
 
 
-# write samples to AWG
-comm.instrument_control.write_samples_Tektronix_AWG70002B(samples, ip_address='192.168.1.21',
-                                                sample_rate=[500e6],
-                                                amp_pp=[0.5], channels=[1], 
-                                                out_filter=['normal'],log_mode=True)
+# # write samples to AWG
+# comm.instrument_control.write_samples_Tektronix_AWG70002B(samples, ip_address='192.168.1.21',
+#                                                 sample_rate=[500e6],
+#                                                 amp_pp=[0.5], channels=[1], 
+#                                                 out_filter=['normal'],log_mode=True)
 
 # Result: The inf will be played by the AWG with maximum amplitude.
 #         The driver should prevent this by not accepting inf values
 #         Positive infinity and negative infinity will be catched by np.isinf()
 
 
-# Test with on signal, maximal length detection  
+#--- Test with on signal, maximal length detection  
 # Maximum Bytes for one command : 999,999,999 Bytes          
 # Bytes per float = 4 Bytes
-# Total length of signal for one command : 249999999 Samples                       
+# Total length of signal for one command : 249999999 Samples  
+                     
 start = time.time()
 print("START")
-n = 150000000
+n = 200000000
 #n = 10000000
 phi = np.arange(n)/n * np.pi*2
 samples = np.array([np.ones(n)])
@@ -95,5 +110,5 @@ comm.instrument_control.write_samples_Tektronix_AWG70002B(samples, ip_address='1
 end = time.time()
 print("END")
 print(end - start)
-'''
+
 
