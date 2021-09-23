74ed7c8fb8023ee091fa904dcb932f1678ee4d0c
Marcel <s30308@beuth-hochschule.de>
2021-09-23 16:39:27 +0200

    Added get_samples_Tektronix_MSO68 to communicate with the scope

--- comm/instrument_control.py
+++ comm/instrument_control.py
@@ -1,9 +1,10 @@
-
+import sys
 import visa
 import numpy as np
 import matplotlib.pyplot as plt
 #import math
 import time
+import logging
 
 
 def get_samples_DLM2034(channels=(1), address='192.168.1.12'):
@@ -206,3 +207,189 @@ def write_samples_AWG33522A(samples, ip_address='192.168.1.44', sample_rate=[250
         
     awg.close() # closing AWG
     rm.close()  # closing resource manager 
+
+
+def get_samples_Tektronix_MSO68(channels=[1], ip_address='192.168.1.20',word_length = 1,log_mode = False):   
+    """
+    get_samples_Tektronix_MSO68
+    
+    Function for reading samples from  Tektronix MSO68 Scope
+
+    Parameters
+    ----------
+    channels : list of integers, optional
+        iterable containing the channel numbers to fetch data samples from device. The default is [1].
+        For more chennels use [1,2,...]
+    address : string, optional
+        IP Adress of device. The default is '192.168.1.20'.
+
+    Returns
+    -------
+    sample_rate : float
+        actual sample rate of returned samples.
+    wfm : list of numpy arrays, float
+        each list element constains the samples of a requested channel as numpy float array.
+
+    """
+
+    # =============================================================================
+    #  Create logger which writes to file
+    # ============================================================================= 
+    # Create logger
+    logger = logging.getLogger(__name__)
+
+    # Set the log level
+    logger.setLevel(logging.INFO)
+
+    # Create file handler and standard output handler (terminal output)
+    file_handler = logging.FileHandler('{0}.log'.format(__name__))
+    if log_mode == False:
+        file_handler.setLevel(51)
+    else:
+        file_handler.setLevel(logging.INFO)
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(logging.ERROR)
+
+    # Set format of the logs with formatter
+    formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(name)s :: Line No %(lineno)d:: %(message)s')
+
+    # Adding formatter to handler
+    file_handler.setFormatter(formatter)
+    stdout_handler.setFormatter(formatter)
+
+    # Adding handler to logger
+    logger.addHandler(file_handler)
+    logger.addHandler(stdout_handler)
+
+    # =============================================================================
+    #  Check inputs of correctnes
+    # ============================================================================= 
+
+    try:
+        if not isinstance(channels, list):
+            raise TypeError('Type of channels mus be list')
+
+        if len(channels) > 4:
+            raise ValueError('To much channels ({0}). The Scope has only 2 output channels'.format(len(channels)))
+
+        if any(ch_number > 4 for ch_number in channels) > 4 or any(ch_number < 1 for ch_number in channels):
+            raise ValueError('Channel numbers must be integer and betwenn 1 and 4')
+
+        if word_length not in [1,2,4]:
+            raise ValueError('Wrong word length. Onyly 1 (signed char), 2 (signed short) or 4 (long) are allowed')
+
+    except Exception as e:
+        logger.error('{0}'.format(e))
+        exit()
+    
+    # =============================================================================
+    #  importing visa for communication with the AWG device
+    # ============================================================================= 
+    # create ressource
+    rm = visa.ResourceManager('@py')
+
+    # open connection to AWG
+    logger.info("Create IP connection with " + str(ip_address))
+    try:
+        scope = rm.open_resource('TCPIP::' + ip_address + '::INSTR')
+    except Exception as e:
+        logger.error('No connection possible. Check TCP/IP connection \n  {0}'.format(e))
+        exit()
+    
+    logger.info("Device properties: " + str(scope.query('*IDN?')))
+
+
+    # =============================================================================
+    #  Settings for the Scope
+    # =============================================================================  
+
+    # Generate waveform vector
+    wfm = []
+
+    # Set the last datapoint of the waveform which will be transmitted. 
+    # If this value is bigger than the actual length of the waveform, the Curve? function catches all samples
+    scope.write('DATA:STOP 62500000')
+
+    # See if scope is running or not
+    if scope.query('ACQuire:STATE?')[0] == '1':
+        # Setting the restart condition of the scope after acqusition - True means restart
+        is_running = True
+
+        # start a single acquisition
+        scope.write('ACQuire:STOPAfter SEQuence')
+        scope.write('ACQuire:STATE RUN')  
+
+        # Loop to ascertain that the scope has finished the acquisition process
+        while scope.query('ACQuire:STATE?')[0] == '1':
+            pass
+
+    else:
+        # Setting the restart condition of the scope after acqusition - False means stay by stop
+        is_running = False
+    
+    # Setting outgoing data format (In this case signed binary data)
+    scope.write('DATA:ENCDG SRIbinary')
+
+    # Get sample rate of the scope
+    sample_rate = scope.query('HORizontal:MODE:SAMPLERate?')
+
+    # Set datatype for acquisition  ( b (signed char),h (signed short) or l (long) )
+    # For information see documentation of struct module
+
+    if word_length == 1:
+        acq_data_type = 'b'
+    elif word_length == 2:
+        acq_data_type = 'h'
+    else:
+        acq_data_type = 'l'
+        
+    logger.info("Used datatype: " + acq_data_type)
+
+    # Read the channels
+    for ch in (channels):
+        # Select waveform source
+        scope.write('DATA:SOURCE CH{0:d}'.format(ch))
+
+        # Setting number of bytes per waveformpoint (sample) 
+        scope.write('WFMOutpre:BYT_Nr {0:d}'.format(word_length))
+
+        # Reading waveform data and write them as numpy array to list
+        #scope.write('Curve?')
+        try:
+            tmp = scope.query_binary_values('Curve?',datatype=acq_data_type ,is_big_endian=False, container=np.array)
+        except Exception as e:
+            logger.error('Channel {0:d} seems not activated \n '.format(ch))
+            exit()
+
+
+        # Reading vertical scaling of the scope (Voltage per div)
+        ver_scale = float(scope.query('CH{0:d}:SCAle?'.format(ch),delay = 0.5))
+
+        # Reading vertical position ( Y-Position on scope screen)
+        ver_position = float(scope.query('CH{0:d}:POSition?'.format(ch),delay = 0.5))
+
+        # Reading offset from scope
+        offset = float(scope.query('CH{0:d}:OFFSet?'.format(ch),delay = 0.5))
+
+        # Scale amplitude
+        #wfm.append(5 * ver_scale / (2 ** (float(word_length) * 7)) * tmp  + ver_offset)
+        # if word_length == 4:
+        #     wfm.append(ver_scale * (tmp - ver_position) + offset)
+        # else:
+        wfm.append(ver_scale * (5 / (2 ** (float(word_length) * 8 - 1)) * tmp - ver_position) + offset)
+    
+
+    # Restart the scope
+    if is_running:
+        scope.write('ACQuire:STOPAfter RUNSTOP')
+        scope.write('ACQuire:STATE RUN')  
+
+
+    # closing scope connection
+    scope.close()
+   
+    # closing resource manager 
+    rm.close()  
+
+    return sample_rate, wfm
