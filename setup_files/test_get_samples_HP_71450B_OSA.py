925a1666c18598392fc4eaa928c4c3a1fcdec34d
L. Molle <Lutz.Molle@HTW-Berlin.de>
2021-10-08 17:13:08 +0200

    Debugged driver
    Tetsted version of the OSA driver

--- setup_files/test_get_samples_HP_71450B_OSA.py
+++ setup_files/test_get_samples_HP_71450B_OSA.py
@@ -3,31 +3,32 @@ import sys
 import os
 if not any(os.path.abspath('..') == p for p in sys.path): 
     print('adding comm module to path...')
-    sys.path.insert(0, os.path.abspath('.'))
-    print(os.path.abspath('.'))
+    sys.path.insert(0, os.path.abspath('..'))
+    print(os.path.abspath('..'))
 import numpy as np  # Version 1.20.1
 import matplotlib.pyplot as plt # Version 3.3.4
 import comm as comm
 
+wanted_trace = 'A'
 
 ##### Test with one trace
 print('-----Test with one trace-----')
-test_one_trace = comm.instrument_control.get_samples_HP_71450B_OSA(traces = ['A'], GPIB_address='4', log_mode=False)
+test_one_trace = comm.instrument_control.get_samples_HP_71450B_OSA(traces = [wanted_trace], GPIB_address='13', log_mode=False, single_sweep = False)
 # Get trace data
-test_one_trace_data = test_one_trace['A']['trace_data']
+test_one_trace_data = test_one_trace[wanted_trace]['Trace_data']
 print('Size of trace data: '+ str(test_one_trace_data.size))
 print('Data type of trace data: '+ str(type(test_one_trace_data)))
 # Get Unit
-test_one_trace_unit = test_one_trace['A']['Unit']
+test_one_trace_unit = test_one_trace[wanted_trace]['Unit']
 print('Unit of trace data: ' + test_one_trace_unit)
 # Get start WL
-test_one_trace_Start_WL = test_one_trace['A']['Start_WL']
+test_one_trace_Start_WL = test_one_trace[wanted_trace]['Start_WL']
 print('Start wavelength: ' + str(test_one_trace_Start_WL)) 
 # Get stop WL
-test_one_trace_Stop_WL = test_one_trace['A']['Start_WL']
+test_one_trace_Stop_WL = test_one_trace[wanted_trace]['Stop_WL']
 print('Stop wavelength: ' + str(test_one_trace_Stop_WL)) 
 # Get wavelength vector
-test_one_trace_WL_vector = test_one_trace['A']['WL_Vector']
+test_one_trace_WL_vector = test_one_trace[wanted_trace]['WL_Vector']
 print('Size of wavelength vector: '+ str(test_one_trace_WL_vector.size))
 print('Data type of wavelength vector: '+ str(type(test_one_trace_WL_vector)))
 # Plot received spectrum
@@ -38,66 +39,65 @@ plt.ylabel('Amplitude in ' + test_one_trace_unit)
 plt.plot(test_one_trace_WL_vector,test_one_trace_data)
 plt.show()
 
-# ##### Test with two traces
-# test_two_traces = comm.instrument_control.get_samples_HP_71450B_OSA(traces = ['A','B'], GPIB_address='4', log_mode=False)
-# print('-----Test with two traces-----')
-# for trace in test_two_traces:
-#     test_two_traces = comm.instrument_control.get_samples_HP_71450B_OSA(traces = ['A'], GPIB_address='4', log_mode=False)
-#     # Get trace data
-#     test_two_traces_data = test_two_traces[trace]['trace_data']
-#     print('Size of trace data: '+ str(test_two_traces_data.size))
-#     print('Data type of trace data: '+ str(type(test_two_traces_data)))
-#     # Get Unit
-#     test_two_traces_unit = test_two_traces[trace]['Unit']
-#     print('Unit of trace data: ' + test_two_traces_unit)
-#     # Get start WL
-#     test_two_traces_Start_WL = test_two_traces[trace]['Start_WL']
-#     print('Start wavelength: ' + str(test_one_trace_Start_WL)) 
-#     # Get stop WL
-#     test_two_traces_Stop_WL = test_two_traces[trace]['Start_WL']
-#     print('Stop wavelength: ' + str(test_one_trace_Stop_WL)) 
-#     # Get wavelength vector
-#     test_two_traces_WL_vector = test_two_traces[trace]['WL_Vector']
-#     print('Size of wavelength vector: '+ str(test_two_traces_WL_vector.size))
-#     print('Data type of wavelength vector: '+ str(type(test_two_traces_WL_vector)))
-#     # Plot received spectrum
-#     plt.figure(2)
-#     plt.title('Test with one trace')
-#     plt.xlabel('Wavelength in nm')
-#     plt.ylabel('Amplitude in ' + test_one_trace_unit)
-#     plt.plot(test_one_trace_WL_vector,test_one_trace_data)
-# plt.show()
+##### Test with two traces
+test_two_traces = comm.instrument_control.get_samples_HP_71450B_OSA(traces = ['A','B'], GPIB_address='13', log_mode=False)
+print('-----Test with two traces-----')
+for trace in test_two_traces:
+    # Get trace data
+    test_two_traces_data = test_two_traces[trace]['Trace_data']
+    print('Size of trace data: '+ str(test_two_traces_data.size))
+    print('Data type of trace data: '+ str(type(test_two_traces_data)))
+    # Get Unit
+    test_two_traces_unit = test_two_traces[trace]['Unit']
+    print('Unit of trace data: ' + test_two_traces_unit)
+    # Get start WL
+    test_two_traces_Start_WL = test_two_traces[trace]['Start_WL']
+    print('Start wavelength: ' + str(test_two_traces_Start_WL)) 
+    # Get stop WL
+    test_two_traces_Stop_WL = test_two_traces[trace]['Stop_WL']
+    print('Stop wavelength: ' + str(test_two_traces_Stop_WL)) 
+    # Get wavelength vector
+    test_two_traces_WL_vector = test_two_traces[trace]['WL_Vector']
+    print('Size of wavelength vector: '+ str(test_two_traces_WL_vector.size))
+    print('Data type of wavelength vector: '+ str(type(test_two_traces_WL_vector)))
+    # Plot received spectrum
+    plt.figure(2)
+    plt.title('Test with two traces')
+    plt.xlabel('Wavelength in nm')
+    plt.ylabel('Amplitude in ' + test_two_traces_unit)
+    plt.plot(test_one_trace_WL_vector,test_two_traces_data)
+plt.show()
 
 
-# ##### Test with three traces
-# test_three_traces = comm.instrument_control.get_samples_HP_71450B_OSA(traces = ['A','B','C'], GPIB_address='4', log_mode=False)
-# print('-----Test with three traces-----')
-# for trace in test_three_traces:
-#     test_three_traces = comm.instrument_control.get_samples_HP_71450B_OSA(traces = ['A'], GPIB_address='4', log_mode=False)
-#     # Get trace data
-#     test_three_traces_data = test_three_traces[trace]['trace_data']
-#     print('Size of trace data: '+ str(test_three_traces_data.size))
-#     print('Data type of trace data: '+ str(type(test_three_traces_data)))
-#     # Get Unit
-#     test_three_traces_unit = test_three_traces[trace]['Unit']
-#     print('Unit of trace data: ' + test_three_traces_unit)
-#     # Get start WL
-#     test_three_traces_Start_WL = test_three_traces[trace]['Start_WL']
-#     print('Start wavelength: ' + str(test_one_trace_Start_WL)) 
-#     # Get stop WL
-#     test_three_traces_Stop_WL = test_three_traces[trace]['Start_WL']
-#     print('Stop wavelength: ' + str(test_one_trace_Stop_WL)) 
-#     # Get wavelength vector
-#     test_three_traces_WL_vector = test_three_traces[trace]['WL_Vector']
-#     print('Size of wavelength vector: '+ str(test_three_traces_WL_vector.size))
-#     print('Data type of wavelength vector: '+ str(type(test_three_traces_WL_vector)))
-#     # Plot received spectrum
-#     plt.figure(2)
-#     plt.title('Test with one trace')
-#     plt.xlabel('Wavelength in nm')
-#     plt.ylabel('Amplitude in ' + test_three_traces_unit)
-#     plt.plot(test_three_traces_WL_vector,test_three_traces_data)
-# plt.show()
+##### Test with three traces
+test_three_traces = comm.instrument_control.get_samples_HP_71450B_OSA(traces = ['A','B','C'], GPIB_address='13', log_mode=False)
+print('-----Test with three traces-----')
+for trace in test_three_traces:
+
+    # Get trace data
+    test_three_traces_data = test_three_traces[trace]['Trace_data']
+    print('Size of trace data: '+ str(test_three_traces_data.size))
+    print('Data type of trace data: '+ str(type(test_three_traces_data)))
+    # Get Unit
+    test_three_traces_unit = test_three_traces[trace]['Unit']
+    print('Unit of trace data: ' + test_three_traces_unit)
+    # Get start WL
+    test_three_traces_Start_WL = test_three_traces[trace]['Start_WL']
+    print('Start wavelength: ' + str(test_one_trace_Start_WL)) 
+    # Get stop WL
+    test_three_traces_Stop_WL = test_three_traces[trace]['Stop_WL']
+    print('Stop wavelength: ' + str(test_one_trace_Stop_WL)) 
+    # Get wavelength vector
+    test_three_traces_WL_vector = test_three_traces[trace]['WL_Vector']
+    print('Size of wavelength vector: '+ str(test_three_traces_WL_vector.size))
+    print('Data type of wavelength vector: '+ str(type(test_three_traces_WL_vector)))
+    # Plot received spectrum
+    plt.figure(2)
+    plt.title('Test with three traces')
+    plt.xlabel('Wavelength in nm')
+    plt.ylabel('Amplitude in ' + test_three_traces_unit)
+    plt.plot(test_three_traces_WL_vector,test_three_traces_data)
+plt.show()
 
 # #### Test with to many traces
 # print('Test with too many traces')
