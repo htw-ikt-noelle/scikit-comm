31d43a6a9dd1676d16b1ce5d30e1588dfad6ff87
Marcel <s30308@beuth-hochschule.de>
2021-11-05 17:15:32 +0100

    Adding some TODOs

--- setup_files/test_set_attenuation_MTA_150.py
+++ setup_files/test_set_attenuation_MTA_150.py
@@ -9,6 +9,7 @@ import comm as comm
 
 # Test 1
 # Checking the default values
+# Expectetd result: The data from cassette 1 should be readed.
 test_1 = comm.instrument_control.set_attenuation_MTA_150()
 print('<---Test 1--->')
 print(test_1)
@@ -16,7 +17,7 @@ print('\n\n')
 
 # Test 2
 # Checking with two cassets
-test_2 = comm.instrument_control.set_attenuation_MTA_150(cassetts=['1','2'],attenuations=[50,50], offsets=[10,10], wavelengths=[1500,1500])
+test_2 = comm.instrument_control.set_attenuation_MTA_150(cassetts=['1','2'],attenuations=[0,0], offsets=[0,0], wavelengths=[1500,1500])
 print('<---Test 2--->')
 print(test_2)
 print('\n\n')
