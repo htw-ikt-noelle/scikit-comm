8fef334cd9e829d1138d1803c2430a94186fb9e0
Marcel <s30308@beuth-hochschule.de>
2021-11-11 11:47:55 +0100

    New test file

--- setup_files/test_set_attenuation_MTA_150.py
+++ setup_files/test_set_attenuation_MTA_150.py
@@ -9,29 +9,62 @@ import comm as comm
 
 # Test 1
 # Checking the default values
-# Expectetd result: The data from cassette 1 should be readed.
+# Expectetd result: The data from cassette 1 should be readed. The properties of the device should not be changed.
 test_1 = comm.instrument_control.set_attenuation_MTA_150()
 print('<---Test 1--->')
-print(test_1)
+for cassette in test_1:
+    print (cassette)
+    for cassette_item in test_1[cassette]:
+        print (cassette_item,':',test_1[cassette][cassette_item])
 print('\n\n')
 
 # Test 2
 # Checking with two cassets
-test_2 = comm.instrument_control.set_attenuation_MTA_150(cassetts=['1','2'],attenuations=[0,0], offsets=[0,0], wavelengths=[1500,1500])
+# Expecated result: Get the data from the cassette 1 and 2. The properties of the device should not be changed.
+test_2 = comm.instrument_control.set_attenuation_MTA_150(cassetts=['1','2'])
 print('<---Test 2--->')
-print(test_2)
+for cassette in test_2:
+    print (cassette)
+    for cassette_item in test_2[cassette]:
+        print (cassette_item,':',test_2[cassette][cassette_item])
 print('\n\n')
-
+3
 # Test 3
-# Checking negativ offset
-test_3 = comm.instrument_control.set_attenuation_MTA_150(cassetts=['1','2'],attenuations=[50,50], offsets=[-10,-10], wavelengths=[1500,1500])
+# Change values of attenuation, offset and wavelength for cassette 2.
+test_3 = comm.instrument_control.set_attenuation_MTA_150(cassetts=['2'],attenuations=[10], offsets=[5], wavelengths=[1300])
 print('<---Test 3--->')
-print(test_3)
+for cassette in test_3:
+    print (cassette)
+    for cassette_item in test_3[cassette]:
+        print (cassette_item,':',test_3[cassette][cassette_item])
 print('\n\n')
 
 # Test 4
-# Checking maximal attenuation
-test_3 = comm.instrument_control.set_attenuation_MTA_150(cassetts=['1','2'],attenuations=[60,60], offsets=[60,60], wavelengths=[1500,1500])
+# Change only attenuation of cassette 2.
+test_4 = comm.instrument_control.set_attenuation_MTA_150(cassetts=['2'],attenuations=[0])
 print('<---Test 4--->')
-print(test_3)
+for cassette in test_4:
+    print (cassette)
+    for cassette_item in test_4[cassette]:
+        print (cassette_item,':',test_4[cassette][cassette_item])
+print('\n\n')
+
+# Test 5
+# Change wavelength attenuation of cassette 2.
+test_5 = comm.instrument_control.set_attenuation_MTA_150(cassetts=['2'],wavelength=[1350])
+print('<---Test 5--->')
+for cassette in test_5:
+    print (cassette)
+    for cassette_item in test_5[cassette]:
+        print (cassette_item,':',test_5[cassette][cassette_item])
+print('\n\n')
+
+# Test 6
+# Change only offset of cassette 2.
+test_6 = comm.instrument_control.set_attenuation_MTA_150(cassetts=['2'],offset=[0])
+print('<---Test 6--->')
+for cassette in test_6:
+    print (cassette)
+    for cassette_item in test_6[cassette]:
+        print (cassette_item,':',test_6[cassette][cassette_item])
 print('\n\n')
