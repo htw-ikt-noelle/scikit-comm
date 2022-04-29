35164b004b7a2c0bccb140bb864be54a25f7250d
Markus Nölle <noelle@htw-berlin.de>
2022-04-29 16:54:13 +0200

    changed osnr method name

--- comm/utils.py
+++ comm/utils.py
@@ -371,7 +371,7 @@ def ber_awgn(value=np.arange(20), modulation_format='QAM', modulation_order=4, t
     return np.asarray(ber)
 
 
-def estimate_snr_spectrum(power_vector = [], wavelength_vector = [], interpolation_points = [], integration_area = [], resolution_bandwidth = 0.1, polynom_order = 3, plotting = False):
+def estimate_osnr_spectrum(power_vector = [], wavelength_vector = [], interpolation_points = [], integration_area = [], resolution_bandwidth = 0.1, polynom_order = 3, plotting = False):
 
     """ 
     Description
