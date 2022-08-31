7b25acd581ef8327e4df6f89465eae9bf6c1d1c1
Markus Nölle <noelle@htw-berlin.de>
2022-08-31 20:14:09 +0200

    updated SOP plot
    * function returns handles to the figure artists
    * for speed up: if figure already exists, only signal data points are updated, not all spheres and labels

--- comm/visualizer.py
+++ comm/visualizer.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import numpy.fft as fft
 import matplotlib.pyplot as plt
@@ -234,10 +233,23 @@ def plot_poincare_sphere(samplesX, samplesY, decimation=1, fNum = 1, tit = 'Poin
 
     Returns
     -------
-    None.
-
+    handles :  dict containing following keys
+        fig : matplotlib.figure.Figure
+            Figure object the signal is plotted to.
+        ax : matplotlib.axes._subplots.Axes3DSubplot
+            Axes object which contains the Poincaré sphere artists
+        line : mpl_tooklits.mplot3d.art3d.Line3d
+            Line object which contains the Stokes parameters (S1, S2, S3).
     """
     
+    # helper function to select the artist labeled with 'SOP'
+    def _isSOP(line):
+        if line.get_label() == 'SOP':
+            return True
+        else:
+            return False
+    
+    # decimate signal
     samplesX = samplesX[0::decimation]
     samplesY = samplesY[0::decimation]
     
@@ -245,60 +257,85 @@ def plot_poincare_sphere(samplesX, samplesY, decimation=1, fNum = 1, tit = 'Poin
     s0 = np.abs(samplesX)**2 + np.abs(samplesY)**2
     s1 = np.abs(samplesX)**2 - np.abs(samplesY)**2
     s2 = 2 * (samplesX * np.conj(samplesY)).real
-    s3 = -2 * (samplesX * np.conj(samplesY)).imag
+    s3 = -2 * (samplesX * np.conj(samplesY)).imag  
     
-    # prepare figure
-    plt.figure(fNum) 
-    ax = plt.axes(projection ='3d')    
-    plt.axis('Off')
-    ax.set_box_aspect([1,1,1])
-    plt.title(tit)
+    # if figure does not exist: plot all artists (Axis, Sphere, labels, etc.)...
+    if fNum not in plt.get_fignums():
     
-    # prepare sphere coordinates    
-    u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:20j]
-    x = np.cos(u) * np.sin(v)
-    y = np.sin(u) * np.sin(v)
-    z = np.cos(v)
-    
-    if labels:
-        # plot sphere
-        ax.plot_surface(x, y, z, alpha=0.2)
-        ax.view_init(elev=15, azim=-65)          
-        ax.set_xlabel('S1')
-        ax.set_ylabel('S2')
-        ax.set_zlabel('S3')
-        # plot three rings
-        ph = np.linspace(0, 2*np.pi, 20)
-        ax.plot3D(np.zeros_like(ph), np.sin(ph), np.cos(ph), 'k')
-        ax.plot3D(np.sin(ph), np.zeros_like(ph), np.cos(ph), 'k')
-        ax.plot3D(np.sin(ph), np.cos(ph), np.zeros_like(ph), 'k')
-        # plot six points (V, H, 45, -45, RCP, LCP)
-        ms = 5
-        ax.plot3D(0, 0, 1, 'ko', markersize=ms)
-        ax.text(0,0,1.2, 'RCP')
-        ax.plot3D(0, 0, -1, 'ko', markersize=ms)
-        ax.text(0,0,-1.2, 'LCP')
-        ax.plot3D(1, 0, 0, 'ko', markersize=ms)
-        ax.text(1.2, 0, 0, 'H')
-        ax.plot3D(-1, 0, 0, 'ko', markersize=ms)
-        ax.text(-1.2, 0, 0, 'V')
-        ax.plot3D(0, 1, 0, 'ko', markersize=ms)
-        ax.text(0, 1.2, 0, '45')
-        ax.plot3D(0, -1, 0, 'ko', markersize=ms)
-        ax.text(0, -1.3, 0, '-45')                     
+        # prepare figure
+        f = plt.figure(fNum) 
+        ax = plt.axes(projection ='3d')    
+        plt.axis('Off')
+        ax.set_box_aspect([1,1,1])
+        plt.title(tit)
+        
+        # prepare sphere coordinates    
+        u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:20j]
+        x = np.cos(u) * np.sin(v)
+        y = np.sin(u) * np.sin(v)
+        z = np.cos(v)
+        
+        if labels:
+            # plot sphere
+            ax.plot_surface(x, y, z, alpha=0.2)
+            ax.view_init(elev=15, azim=-65)          
+            ax.set_xlabel('S1')
+            ax.set_ylabel('S2')
+            ax.set_zlabel('S3')
+            # plot three rings
+            ph = np.linspace(0, 2*np.pi, 20)
+            ax.plot3D(np.zeros_like(ph), np.sin(ph), np.cos(ph), 'k')
+            ax.plot3D(np.sin(ph), np.zeros_like(ph), np.cos(ph), 'k')
+            ax.plot3D(np.sin(ph), np.cos(ph), np.zeros_like(ph), 'k')
+            # plot six points (V, H, 45, -45, RCP, LCP)
+            ms = 5
+            ax.plot3D(0, 0, 1, 'ko', markersize=ms)
+            ax.text(0,0,1.2, 'RCP')
+            ax.plot3D(0, 0, -1, 'ko', markersize=ms)
+            ax.text(0,0,-1.2, 'LCP')
+            ax.plot3D(1, 0, 0, 'ko', markersize=ms)
+            ax.text(1.2, 0, 0, 'H')
+            ax.plot3D(-1, 0, 0, 'ko', markersize=ms)
+            ax.text(-1.2, 0, 0, 'V')
+            ax.plot3D(0, 1, 0, 'ko', markersize=ms)
+            ax.text(0, 1.2, 0, '45')
+            ax.plot3D(0, -1, 0, 'ko', markersize=ms)
+            ax.text(0, -1.3, 0, '-45')                     
+        else:
+            # plot sphere
+            ax.view_init(elev=25, azim=45)        
+            ax.plot_wireframe(x, y, z, alpha=0.1, color='k')
+            # plot axis
+            len = 1.8
+            ax.quiver(0, 0,0, 0, 0, len, color='k')
+            ax.quiver(0, 0, 0, len, 0, 0, color='k')
+            ax.quiver(0, 0, 0, 0, len, 0, color='k')
+            ax.text(len*1.2, 0, 0, 'S1', size=15)
+            ax.text(0, len*1.2, 0, 'S2', size=15)
+            ax.text(0, 0, len, 'S3', size=15)           
+        
+        # plot data and label line artist as 'SOP'
+        li = ax.plot3D(s1/s0, s2/s0, s3/s0, '.b', label='SOP')   
+        plt.show()
+    # ...otherwise only update data in figure (for speed)
     else:
-        # plot sphere
-        ax.view_init(elev=25, azim=45)        
-        ax.plot_wireframe(x, y, z, alpha=0.1, color='k')
-        # plot axis
-        len = 1.8
-        ax.quiver(0, 0, 0, 0, 0, len, color='k')
-        ax.quiver(0, 0, 0, len, 0, 0, color='k')
-        ax.quiver(0, 0, 0, 0, len, 0, color='k')
-        ax.text(len*1.2, 0, 0, 'S1', size=15)
-        ax.text(0, len*1.2, 0, 'S2', size=15)
-        ax.text(0, 0, len, 'S3', size=15)           
+        # get all required handles
+        f = plt.figure(fNum)
+        ax = f.axes[0]
+        # find all artists labeled as 'SOP'
+        li = ax.findobj(_isSOP)
+        # update data
+        li[0].set_xdata(s1/s0)
+        li[0].set_ydata(s2/s0)
+        li[0].set_3d_properties(s3/s0)
+        # update plot
+        f.canvas.draw()
+        # wait for figure to update
+        plt.pause(0.1)        
     
-    # plot data
-    ax.plot3D(s1/s0, s2/s0, s3/s0, '.b')   
-    plt.show()
+    # prepare return params
+    handles = dict()
+    handles['fig'] = f
+    handles['ax'] = ax
+    handles['line'] = li[0]
+    return handles
