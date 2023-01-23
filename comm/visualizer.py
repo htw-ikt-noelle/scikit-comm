ea9dd2774fb7dd5234c430eed40cf0f354aa3f2a
Markus Nölle <noelle@htw-berlin.de>
2023-01-23 19:02:56 +0100

    added method and clean up

--- comm/visualizer.py
+++ comm/visualizer.py
@@ -2,6 +2,7 @@ import numpy as np
 import numpy.fft as fft
 import matplotlib.pyplot as plt
 import copy
+import tkinter as tk
 
 from . import utils
 
@@ -532,3 +533,124 @@ def plot_poincare_sphere(samplesX, samplesY, decimation=1, fNum=1,
     handles['ax'] = ax
     handles['line'] = li[0]
     return handles
+
+
+def place_figures(auto_layout=True, offset=[0,0], screen_resolution=None, nc=4, 
+                  nr=3, taskbar_offset=40, figure_toolbar=64):
+    """
+    Place open figures on screen unsing specified layout. Basic programmatic
+    idea taken from [1].
+    
+    [1] JaeJun Lee (2023). automatically arrange figure windows 
+    (https://www.mathworks.com/matlabcentral/fileexchange/48480-automatically-arrange-figure-windows), 
+    MATLAB Central File Exchange. Retrieved January 23, 2023. 
+    
+        Parameters
+    ----------
+    auto_layout : bool, optional
+        The layout is chosen automatically depending on the number of opened 
+        figures. Number of figures must not exceed 32. The default is True.
+    offset : list, optional
+        Specifies the offset from top left screen edge in integer pixels [x, y],
+        where the layout is supposed to start. This can be used to put the layout
+        onto the second screen, if available. The default is [0,0].
+    screen_resolution : list, optional
+        Specifies the resolution in integer pixels [width,height] which is used 
+        for the layout of the figures. If None, the resolution of the whole screen
+        (using the specified offset) is estimated. The default is None.
+    nc : int, optional
+        Number of coloums used for the layout. Only used if auto_layout=False.
+        The default is 4.
+    nr : int, optional
+        Number of rows used for the layout. Only used if auto_layout=False.
+        The default is 3.
+    taskbar_offset : int, optional
+        Height of the (windows) taskbar which should not be covered by the layout. 
+        The taskbar is assumed to be on the bottom of the screen. The default is 40.
+    figure_toolbar : int, optional
+        Height of the toolbar of the individual plot windows. The default is 64.
+
+    Returns
+    -------
+    None.
+
+    """      
+    
+    # get figure handles
+    figHandle = list(map(plt.figure, plt.get_fignums()))   
+    n_fig = len(figHandle)
+
+    if n_fig <= 0:
+        raise ValueError('no figures found to place')
+
+    if screen_resolution:
+        screen_resolution[1] = screen_resolution[1] -  taskbar_offset
+    else:    
+        # workaround to determine screen resolution:
+        # * open tk window
+        # * place the window according to offset
+        # * make it fullscreen
+        # * get width and height
+        # * kill window
+        # from https://stackoverflow.com/questions/3129322/how-do-i-get-monitor-resolution-in-python
+        # TODO: find better way to determine screen resolutions of individual monitors
+        root = tk.Tk()
+        root.update_idletasks()
+        root.geometry(f'100x100+{offset[0]}+{offset[1]}')
+        # print(root.geometry())
+        root.attributes('-fullscreen', True)
+        # print(root.geometry())
+        root.state('iconic')
+        screen_resolution = []
+        screen_resolution.append(root.winfo_screenwidth())
+        # reduce screen height by the (windows) taskbar
+        screen_resolution.append(root.winfo_screenheight() -  taskbar_offset)
+        root.destroy()
+
+    # auto layout?
+    if auto_layout:
+        grid = [
+            [1,1],[1,2],
+            [2,2],[2,2],
+            [2,3],[2,3],
+            [3,3],[3,3],[3,3],
+            [3,4],[3,4],[3,4],
+            [4,4],[4,4],[4,4],[4,4],
+            [4,5],[4,5],[4,5],[4,5],
+            [4,6],[4,6],[4,6],[4,6],
+            [4,7],[4,7],[4,7],[4,7],
+            [4,8],[4,8],[4,8],[4,8]
+            ]
+       
+        if n_fig > len(grid)*2:
+            raise ValueError('more figures opened than layout options available')        
+        
+        # portrait mode
+        if screen_resolution[0] < screen_resolution[1]:
+            nc = grid[n_fig-1][0]
+            nr = grid[n_fig-1][1]
+        # landscape mode
+        else:
+            nc = grid[n_fig-1][1]
+            nr = grid[n_fig-1][0]
+    # manual layout
+    else:
+        if (nc * nr) < n_fig:
+            raise ValueError(f'more figures opened ({n_fig}) than rows times coloumns given ({nc*nr}): try to increase numbers or switch to auto layout mode')
+            
+
+    fig_width = screen_resolution[0]/nc 
+    fig_height = screen_resolution[1]/nr - figure_toolbar 
+
+    fig_cnt = 0
+    for r in range(nr):
+        for c in range(nc):
+            if fig_cnt >= n_fig:
+                break        
+            figHandle[fig_cnt].set_figheight(fig_height / figHandle[fig_cnt].get_dpi())
+            figHandle[fig_cnt].set_figwidth(fig_width / figHandle[fig_cnt].get_dpi())
+            if r == 0:
+                figHandle[fig_cnt].canvas.manager.window.move(int(fig_width*c + offset[0]), int(fig_height*r) + offset[1])
+            else:
+                figHandle[fig_cnt].canvas.manager.window.move(int(fig_width*c + offset[0]), int((fig_height+figure_toolbar)*r) + offset[1])
+            fig_cnt += 1
